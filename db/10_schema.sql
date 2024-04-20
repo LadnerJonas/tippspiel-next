b9806c52d7945e19b118a229f7336d9e34e67c70
LadnerJonas <jonas.ladner@tum.de>
2024-04-20 13:27:28 +0200

    improve generate_sneak_preview_leaderboard query by adding UserTotalPoints

--- db/10_schema.sql
+++ db/10_schema.sql
@@ -1,9 +1,9 @@
-drop table if exists Bet CASCADE ;
-drop table if exists CommunityMember CASCADE;
-drop table if exists Community CASCADE;
-drop table if exists PinnedUser CASCADE;
-drop table if exists Game CASCADE;
-drop table if exists "User" CASCADE;
+DROP TABLE IF EXISTS Bet CASCADE;
+DROP TABLE IF EXISTS CommunityMember CASCADE;
+DROP TABLE IF EXISTS Community CASCADE;
+DROP TABLE IF EXISTS PinnedUser CASCADE;
+DROP TABLE IF EXISTS Game CASCADE;
+DROP TABLE IF EXISTS "User" CASCADE;
 
 -- Create the "User" table
 CREATE TABLE "User" (
@@ -12,10 +12,10 @@ CREATE TABLE "User" (
                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create TABLE PinnedUser (
-                        user_id INT REFERENCES "User"(id),
-                        pinned_user_id INT REFERENCES "User"(id),
-                        PRIMARY KEY (user_id, pinned_user_id)
+CREATE TABLE PinnedUser (
+                            user_id INT REFERENCES "User"(id),
+                            pinned_user_id INT REFERENCES "User"(id),
+                            PRIMARY KEY (user_id, pinned_user_id)
 );
 
 -- Create the Community table
@@ -25,10 +25,11 @@ CREATE TABLE Community (
                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-Create TABLE CommunityMember (
-                           id SERIAL PRIMARY KEY,
-                           community_id INT REFERENCES Community(id),
-                           user_id INT REFERENCES "User"(id)
+-- Create the CommunityMember table
+CREATE TABLE CommunityMember (
+                                 id SERIAL PRIMARY KEY,
+                                 community_id INT REFERENCES Community(id),
+                                 user_id INT REFERENCES "User"(id)
 );
 
 -- Create the Game table
@@ -56,22 +57,22 @@ CREATE TABLE Bet (
 
 -- Create a function to calculate and update points_earned in Bet table
 CREATE OR REPLACE FUNCTION update_bet_points()
-RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS $$
 BEGIN
-  -- Calculate points_earned based on the difference between home_score and away_score
-UPDATE Bet b
-SET points_earned =
-        CASE
-            WHEN NEW.home_score = home_team_goals AND NEW.away_score = away_team_goals THEN 8  -- Exact match
-            WHEN NEW.home_score - NEW.away_score = home_team_goals - away_team_goals THEN 6    -- Correct goal difference
-            WHEN (NEW.home_score > NEW.away_score AND home_team_goals > away_team_goals) OR
-                 (NEW.home_score = NEW.away_score AND home_team_goals = away_team_goals) OR
-                 (NEW.home_score < NEW.away_score AND home_team_goals < away_team_goals) THEN 4 -- Correct tendency
-            ELSE 0 -- Incorrect bet
-            END
-WHERE game_id = NEW.id;
+    -- Calculate points_earned based on the difference between home_score and away_score
+    UPDATE Bet b
+    SET points_earned =
+            CASE
+                WHEN NEW.home_score = b.home_team_goals AND NEW.away_score = b.away_team_goals THEN 8  -- Exact match
+                WHEN NEW.home_score - NEW.away_score = b.home_team_goals - b.away_team_goals THEN 6    -- Correct goal difference
+                WHEN (NEW.home_score > NEW.away_score AND b.home_team_goals > b.away_team_goals) OR
+                     (NEW.home_score = NEW.away_score AND b.home_team_goals = b.away_team_goals) OR
+                     (NEW.home_score < NEW.away_score AND b.home_team_goals < b.away_team_goals) THEN 4 -- Correct tendency
+                ELSE 0 -- Incorrect bet
+                END
+    WHERE game_id = NEW.id;
 
-RETURN NEW;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -79,34 +80,49 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER update_bet_points_trigger
     AFTER UPDATE OF home_score, away_score ON Game
     FOR EACH ROW
-    EXECUTE FUNCTION update_bet_points();
+EXECUTE FUNCTION update_bet_points();
 
 -- Create a trigger to prevent bets after the game has started
 CREATE OR REPLACE FUNCTION check_bet_before_game_start()
-RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS $$
 BEGIN
-  IF NEW.created_at > (SELECT start_time FROM Game WHERE id = NEW.game_id) THEN
-    RAISE EXCEPTION 'Bet cannot be placed after the game has started!';
-END IF;
-RETURN NEW;
+    IF NEW.created_at > (SELECT start_time FROM Game WHERE id = NEW.game_id) THEN
+        RAISE EXCEPTION 'Bet cannot be placed after the game has started!';
+    END IF;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER check_bet_before_game_start_trigger
     BEFORE INSERT OR UPDATE ON Bet
     FOR EACH ROW
-    EXECUTE FUNCTION check_bet_before_game_start();
+EXECUTE FUNCTION check_bet_before_game_start();
 
 -- Create the view to get the total score of each user in a community
-CREATE VIEW CommunityUserScores AS
-SELECT CM.community_id,
-       CM.user_id,
-       U.username,
-       COALESCE(SUM(B.points_earned), 0) AS total_score
-FROM CommunityMember CM
-         JOIN "User" U ON CM.user_id = U.id
-         LEFT JOIN Bet B ON CM.user_id = B.user_id
-GROUP BY CM.community_id, CM.user_id, U.username;
+CREATE MATERIALIZED VIEW UserTotalPoints AS
+SELECT
+    u.id AS user_id,
+    u.username,
+    CAST(COALESCE(SUM(B.points_earned), 0) AS INT) AS total_points
+FROM
+    "User" u
+        LEFT JOIN
+    Bet b ON u.id = b.user_id
+GROUP BY
+    u.id, u.username;
+
+-- Create a trigger to update the materialized view on each transaction
+CREATE OR REPLACE FUNCTION update_user_total_points()
+    RETURNS TRIGGER AS $$
+BEGIN
+    REFRESH MATERIALIZED VIEW UserTotalPoints;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_user_total_points_trigger
+    AFTER UPDATE OF points_earned ON Bet
+EXECUTE FUNCTION update_user_total_points();
 
 -- Create the view for Community Leaderboards for all users
 CREATE OR REPLACE VIEW  CommunityLeaderboard_AllUsers AS
@@ -115,12 +131,14 @@ WITH RankedUsers AS (
         CM.community_id,
         CM.user_id,
         U.username,
-        COALESCE(SUM(B.points_earned), 0) AS total_score,
-        RANK() OVER (PARTITION BY CM.community_id ORDER BY COALESCE(SUM(B.points_earned), 0) DESC, U.created_at) AS rank
+        CAST(COALESCE(SUM(B.points_earned), 0) AS INT) AS total_score,
+        CAST(RANK() OVER (PARTITION BY CM.community_id ORDER BY COALESCE(SUM(B.points_earned), 0) DESC, U.created_at) AS INT) AS rank
     FROM
         CommunityMember CM
-            JOIN "User" U ON CM.user_id = U.id
-            LEFT JOIN Bet B ON CM.user_id = B.user_id
+            JOIN
+        "User" U ON CM.user_id = U.id
+            LEFT JOIN
+        Bet B ON CM.user_id = B.user_id
     GROUP BY
         CM.community_id,
         CM.user_id,
@@ -137,7 +155,6 @@ FROM
     RankedUsers RU;
 
 -- Create a function to generate the sneak preview of Community Leaderboards
--- DROP IF EXISTS FUNCTION generate_sneak_preview_leaderboard(integer,integer);
 CREATE OR REPLACE FUNCTION generate_sneak_preview_leaderboard(p_community_id INT, logged_in_user_id INT)
     RETURNS TABLE (
                       f_username VARCHAR(255),
@@ -153,19 +170,17 @@ BEGIN
             SELECT
                 CM.user_id,
                 U.username,
-                CAST(COALESCE(SUM(B.points_earned), 0) AS INT) AS total_points,
-                CAST(ROW_NUMBER() OVER (ORDER BY COALESCE(SUM(B.points_earned), 0) DESC, U.created_at, username) AS INT) AS ranked_user_position,
-                CAST(RANK() OVER (ORDER BY COALESCE(SUM(B.points_earned), 0) DESC, U.created_at) AS INT) AS rank
+                UT.total_points,
+                CAST(ROW_NUMBER() OVER (ORDER BY UT.total_points DESC, U.created_at) AS INT) AS ranked_user_position,
+                CAST(RANK() OVER (ORDER BY UT.total_points DESC, U.created_at) AS INT) AS rank
             FROM
                 CommunityMember CM
-                    JOIN "User" U ON CM.user_id = U.id
-                    LEFT JOIN Bet B ON CM.user_id = B.user_id
+                    JOIN
+                "User" U ON CM.user_id = U.id
+                    LEFT JOIN
+                UserTotalPoints UT ON CM.user_id = UT.user_id
             WHERE
                 CM.community_id = p_community_id
-            GROUP BY
-                CM.user_id,
-                U.username,
-                U.created_at
         ),
              UserPosition AS (
                  SELECT
@@ -180,7 +195,7 @@ BEGIN
                      user_id = logged_in_user_id
              ),
              MaxPosition AS (
-                 SELECT CAST(MAX(ranked_user_position) AS INT) AS max_rup FROM RankedUsers
+                 SELECT MAX(ranked_user_position) AS max_rup FROM RankedUsers
              ),
              PreviewUsers AS (
                  /* logged in user */
@@ -212,16 +227,18 @@ BEGIN
                  SELECT *
                  FROM RankedUsers RU WHERE (SELECT max_rup FROM MaxPosition) <= 7
              )
-        SELECT PU.username AS f_username,
-               PU.total_points AS f_total_points,
-               PU.ranked_user_position AS f_ranked_user_position,
-               PU.rank AS f_rank
-        FROM PreviewUsers PU
-        ORDER BY ranked_user_position;
+        SELECT
+            PU.username AS f_username,
+            PU.total_points AS f_total_points,
+            PU.ranked_user_position AS f_ranked_user_position,
+            PU.rank AS f_rank
+        FROM
+            PreviewUsers PU
+        ORDER BY
+            ranked_user_position;
 END;
 $$
     LANGUAGE plpgsql;
-
 
 -- Indexes for faster retrieval
 -- Create index on User.id and User.username
@@ -235,4 +252,3 @@ CREATE INDEX idx_bet_user_id ON Bet(user_id);
 
 -- Create index on Bet.game_id
 CREATE INDEX idx_bet_game_id ON Bet(game_id);
-
